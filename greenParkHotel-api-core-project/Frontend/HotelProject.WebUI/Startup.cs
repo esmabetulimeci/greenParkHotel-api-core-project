83f0484067d7ca5425d99b6aca795f4bcdd4fd93
esmaimeci <imeciesma@gmail.com>
2024-03-07 20:23:38 +0300

    feat: login was created

--- greenParkHotel-api-core-project/Frontend/HotelProject.WebUI/Startup.cs
+++ greenParkHotel-api-core-project/Frontend/HotelProject.WebUI/Startup.cs
@@ -1,8 +1,11 @@
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.EntityLayer.Concrete;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +25,8 @@ namespace HotelProject.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Context>();
+            services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<Context>();
             services.AddHttpClient();
             services.AddControllersWithViews();
             services.AddAutoMapper(typeof(Startup));
